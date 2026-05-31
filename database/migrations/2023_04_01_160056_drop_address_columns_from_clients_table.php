e37718ec3409435f998b75008ddb97b1992c4283
checkinn71-lab <checkinn71-lab@users.noreply.github.com>
2026-05-31 12:27:28 +0530

    Initial pharmacy management system

---
+++ database/migrations/2023_04_01_160056_drop_address_columns_from_clients_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            if (DB::connection()->getDriverName() !== 'sqlite') {
+                $table->dropForeign('clients_area_id_foreign');
+            }
+            $table->dropColumn([
+                'area_id',
+                'street_name',
+                'building_no',
+                'floor_number',
+                'flat_number',
+                'is_main',
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->unsignedBigInteger('area_id');
+            $table->foreign('clients_area_id_foreign')
+                ->references('id')
+                ->on('area');
+            $table->type('area_id');
+            $table->type('street_name');
+            $table->type('building_no');
+            $table->type('floor_number');
+            $table->type('flat_number');
+            $table->type('is_main');
+        });
+    }
+};
